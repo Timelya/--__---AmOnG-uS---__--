d604e670d5b7cf733c981cecadf6d4c124dda4cb
Timelya <114166697+Timelya@users.noreply.github.com>
2023-04-03 10:34:13 +0200

    Szin váltós háttér hozzáadva

--- kód/pages/_app.tsx
+++ kód/pages/_app.tsx
@@ -2,6 +2,7 @@
 import '../styles/globals.css'
 import '../styles/nav.css'
 import '../styles/styles.css'
+import '../styles/DarkMode.css'
 //java
 import '../java/nav.js'
 import type { AppProps } from 'next/app'
