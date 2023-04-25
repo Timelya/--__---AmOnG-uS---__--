eea4a088738c58f04d27e6f2f84da0180c085a9f
patakitamas2002 <patakitamas2002@gmail.com>
2023-04-25 10:32:27 +0200

    Prisma fix

---
+++ kód/pages/invites.tsx
@@ -0,0 +1,53 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useState } from "react";
+import { prisma } from "../lib/prisma";
+import { useRouter } from "next/router";
+import Navbar from "./Components/Navbar";
+import EventCard from "./Components/EventCard";
+import { GetServerSideProps } from "next";
+import DarkMode from "./Components/DarkMode";
+import EventAdd from "./Components/eventAdd";
+
+interface Event {
+  events: {
+    id: string;
+    name: string;
+    start: number;
+    end: number;
+  }[];
+}
+function Home(events: Event) {
+  return (
+    <>
+      <div>
+        <Head>
+          <title>Rendezvény lista</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Navbar />
+        {
+          events.events.map(x => <EventCard key={x.id} name={x.name} />)
+        }
+        <DarkMode />
+      </div>
+    </>
+  );
+}
+export const getServerSideProps: GetServerSideProps = async () => {
+  let events = await prisma?.event.findMany({
+    select: {
+      id: true,
+      name: true,
+      start: true,
+      end: true
+    },
+  });
+  events = JSON.parse(JSON.stringify(events))
+  return {
+    props: {
+      events,
+    },
+  };
+}
+export default Home;
